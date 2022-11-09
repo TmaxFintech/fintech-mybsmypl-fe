08bc469d26b24ed23c6aff5969911229f10501c5
Seolyu <leeseolyu@gmail.com>
2022-11-09 10:04:24 +0900

    [Chore] modify css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import Certification from './pages/certification/certification';
 
+function setScreenSize() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`); //"--vh"라는 속성으로 정의해준다.
+}
+
+window.addEventListener('resize', () => setScreenSize());
+
 function App() {
   return (
     <div className="web">
